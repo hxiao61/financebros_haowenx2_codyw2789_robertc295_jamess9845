d6357f23489b898d191a3b5dbebde4503ff0fab3
hxiao61 <haowenx2@nycstudents.net>
2026-05-18 05:35:13 -0400

    watchlist

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,9 +1,33 @@
-windows.onload = function(){
-
+window.onload = function(){
+    loadWatchlist()
+    var fivemin = 5 * 60 * 1000
+    setInterval(loadWatchlist, fivemin)
 }
 
 function loadWatchlist(){
+    fetch("/api/watchlist")
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data){
+        var tickerList = data.tickers
+        var tableBody = document.getElementById("watchlist-body")
+        tableBody.innerHTML = ""
 
+        for(var i = 0; i < tickerList.length; i++){
+            var ticker = tickerList[i]
+
+            fetch("/api/stocks/price?ticker=" + ticker)
+            .then(function(response){
+                return response.json()
+            })
+            .then(function(stockData) {
+                var newRow = document.createElement("tr")
+                newRow.innerHTML = "<td>" + stockData.ticker + "</td>" + "<td>$" + stockData.price + "</td>" + "<td><button onclick='removeStock(\"" + stockData.ticker + "\")'>Remove</button></td>"
+                tableBody.appendChild(newRow)
+        })
+    }
+    })
 }
 
 function addStock() {
@@ -15,5 +39,5 @@ function removeStock(ticker){
 }
 
 function searchStock() {
-    
+
 }
